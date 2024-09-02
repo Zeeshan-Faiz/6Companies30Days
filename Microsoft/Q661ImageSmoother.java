4df99867f656192255db5dff1d7aceac401fc5a9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-02 19:28:59 +0530

    Added Q661ImageSmoother code

--- Microsoft/Q661ImageSmoother.java
+++ Microsoft/Q661ImageSmoother.java
@@ -25,8 +25,8 @@ For the points (0,1), (1,0), (1,2), (2,1): floor((200+200+50+200+100+100)/6) = f
 For the point (1,1): floor((50+200+200+200+200+100+100+100+100)/9) = floor(138.888889) = 138
 */
 
-public class Q661ImageSmoother{
-    
+public class Q661ImageSmoother {
+
     public int[][] imageSmoother(int[][] img) {
         int n = img.length;
         int m = img[0].length;
@@ -39,5 +39,57 @@ public class Q661ImageSmoother{
             }
         }
         return ans;
+    }
+
+    static int helper(int arr[][], int i, int j) {
+        
+        int n = arr.length, m = arr[0].length;
+        int count = 1;
+        int sum = arr[i][j];
+
+        // Up Element
+        if (i - 1 >= 0) {
+            sum += arr[i - 1][j];
+            count++;
+            // upper left diagonal
+            if (j - 1 >= 0) {
+                sum += arr[i - 1][j - 1];
+                count++;
+            }
+            // upper right diagonal
+            if (j + 1 < m) {
+                sum += arr[i - 1][j + 1];
+                count++;
+            }
+        }
+
+        // Down Element
+        if (i + 1 < n) {
+            sum += arr[i + 1][j];
+            count++;
+            // Left down diagonal
+            if (j - 1 >= 0) {
+                sum += arr[i + 1][j - 1];
+                count++;
+            }
+            // Right down diagonal
+            if (j + 1 < m) {
+                sum += arr[i + 1][j + 1];
+                count++;
+            }
+        }
+        // Right Element
+        if (j + 1 < m) {
+            sum += arr[i][j + 1];
+            count++;
+        }
+        // left element
+        if (j - 1 >= 0) {
+            sum += arr[i][j - 1];
+            count++;
+        }
+
+        // return average
+        return sum / count;
     }
 }
