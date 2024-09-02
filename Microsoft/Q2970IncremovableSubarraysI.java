0d1346fc9e072fc3d9e174e94fe8848d42058945
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-02 22:04:24 +0530

    Added Q2970IncremovableSubarraysI code

--- Microsoft/Q2970IncremovableSubarraysI.java
+++ Microsoft/Q2970IncremovableSubarraysI.java
@@ -9,7 +9,20 @@ Return the total number of incremovable subarrays of nums. Note that an empty ar
 A subarray is a contiguous non-empty sequence of elements within an array.
 
 Example 1:
+Input: nums = [1,2,3,4]
+Output: 10
+Explanation: The 10 incremovable subarrays are: [1], [2], [3], [4], [1,2], [2,3], [3,4], [1,2,3], [2,3,4], and [1,2,3,4], because on removing any one of these subarrays nums becomes strictly increasing. Note that you cannot select an empty subarray.
 
+Example 2:
+Input: nums = [6,5,7,8]
+Output: 7
+Explanation: The 7 incremovable subarrays are: [5], [6], [5,7], [6,5], [5,7,8], [6,5,7] and [6,5,7,8].
+It can be shown that there are only 7 incremovable subarrays in nums.
+
+Example 3:
+Input: nums = [8,7,6,6]
+Output: 3
+Explanation: The 3 incremovable subarrays are: [8,7,6], [7,6,6], and [8,7,6,6]. Note that [8,7] is not an incremovable subarray because after removing [8,7] nums becomes [6,6], which is sorted in ascending order but not strictly increasing.
 */
 
 public class Q2970IncremovableSubarraysI {
