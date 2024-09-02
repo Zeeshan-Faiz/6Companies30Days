d3d961134574ccc0af8db632c86a23695c3ecab6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-02 18:26:44 +0530

    Added Q661ImageSmoother code

--- Microsoft/Q661ImageSmoother.java
+++ Microsoft/Q661ImageSmoother.java
@@ -1,5 +1,15 @@
 package Microsoft;
 
+/*
+An image smoother is a filter of the size 3 x 3 that can be applied to each cell of an image by 
+rounding down the average of the cell and the eight surrounding cells (i.e., the average of the 
+nine cells in the blue smoother). If one or more of the surrounding cells of a cell is not present, 
+we do not consider it in the average (i.e., the average of the four cells in the red smoother).
+
+Example 1:
+
+*/
+
 public class Q661ImageSmoother{
     
 }
