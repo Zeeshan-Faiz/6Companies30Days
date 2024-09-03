de565795238bdaa2bdb6eaa75d97d567d222a8c4
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-03 19:14:20 +0530

    Added Q453MinMovesToEqualArrayElements code

--- Microsoft/Q453MinMovesToEqualArrayElements.java
+++ Microsoft/Q453MinMovesToEqualArrayElements.java
@@ -1,5 +1,13 @@
 package Microsoft;
 
+/*
+Given an integer array nums of size n, return the minimum number of moves required to make all 
+array elements equal. In one move, you can increment n - 1 elements of the array by 1.
+
+Example 1:
+
+*/
+
 public class Q453MinMovesToEqualArrayElements {
     
 }
