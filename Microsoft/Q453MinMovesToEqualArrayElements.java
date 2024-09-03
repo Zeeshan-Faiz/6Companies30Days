aa470ca7c2ee978cd293b1a88d7ad7df4772fcba
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-03 19:14:33 +0530

    Added Q453MinMovesToEqualArrayElements code

--- Microsoft/Q453MinMovesToEqualArrayElements.java
+++ Microsoft/Q453MinMovesToEqualArrayElements.java
@@ -5,7 +5,13 @@ Given an integer array nums of size n, return the minimum number of moves requir
 array elements equal. In one move, you can increment n - 1 elements of the array by 1.
 
 Example 1:
+Output: 3
+Explanation: Only three moves are needed (remember each move increments two elements):
+[1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
 
+Example 2:
+Input: nums = [1,1,1]
+Output: 0
 */
 
 public class Q453MinMovesToEqualArrayElements {
