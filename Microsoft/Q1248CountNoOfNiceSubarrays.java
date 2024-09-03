fa71cf6f594d4089d91e90a404c143cd0bea6e7d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-03 22:23:58 +0530

    Added Q1248CountNoOfNiceSubarrays code

--- Microsoft/Q1248CountNoOfNiceSubarrays.java
+++ Microsoft/Q1248CountNoOfNiceSubarrays.java
@@ -20,5 +20,19 @@ Output: 16
 */
 
 public class Q1248CountNoOfNiceSubarrays {
-    
+
+    public int numberOfSubarrays(int[] nums, int k) {
+
+        int n = nums.length, ans = 0, t = 0;
+        int[] cnt = new int[n + 1];
+        cnt[0] = 1;
+        for (int v : nums) {
+            t += v & 1;
+            if (t - k >= 0) {
+                ans += cnt[t - k];
+            }
+            cnt[t]++;
+        }
+        return ans;
+    }
 }
