e21ea7057d61d0e9be5da670d3361571ce076bf9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-03 22:23:07 +0530

    Added Q1248CountNoOfNiceSubarrays code

--- Microsoft/Q1248CountNoOfNiceSubarrays.java
+++ Microsoft/Q1248CountNoOfNiceSubarrays.java
@@ -1,5 +1,13 @@
 package Microsoft;
 
+/*
+Given an array of integers nums and an integer k. A continuous subarray is called nice if there 
+are k odd numbers on it. Return the number of nice sub-arrays.
+
+Example 1:
+
+*/
+
 public class Q1248CountNoOfNiceSubarrays {
     
 }
