1053eff705f800e539ef755937d32ecc41a92973
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-04 19:08:59 +0530

    Added Q1334FindCityWithMinNeighborsAtThreshold code

--- Microsoft/Q1334FindCityWithMinNeighborsAtThreshold.java
+++ Microsoft/Q1334FindCityWithMinNeighborsAtThreshold.java
@@ -22,7 +22,16 @@ City 3 -> [City 1, City 2]
 Cities 0 and 3 have 2 neighboring cities at a distanceThreshold = 4, but we have to return city 3 since it has the greatest number.
 
 Example 2:
-
+Input: n = 5, edges = [[0,1,2],[0,4,8],[1,2,3],[1,4,2],[2,3,1],[3,4,1]], distanceThreshold = 2
+Output: 0
+Explanation: The figure above describes the graph. 
+The neighboring cities at a distanceThreshold = 2 for each city are:
+City 0 -> [City 1] 
+City 1 -> [City 0, City 4] 
+City 2 -> [City 3, City 4] 
+City 3 -> [City 2, City 4]
+City 4 -> [City 1, City 2, City 3] 
+The city 0 has 1 neighboring city at a distanceThreshold = 2.
 */
 
 public class Q1334FindCityWithMinNeighborsAtThreshold {
