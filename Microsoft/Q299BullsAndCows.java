cf6d70fa1d9770eef3d5c5701700d20d227d5b93
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-04 10:44:39 +0530

    Added Q299BullsAndCows code

--- Microsoft/Q299BullsAndCows.java
+++ Microsoft/Q299BullsAndCows.java
@@ -15,7 +15,21 @@ guess. The hint should be formatted as "xAyB", where x is the number of bulls an
 of cows. Note that both secret and guess may contain duplicate digits.
 
 Example 1:
+Input: secret = "1807", guess = "7810"
+Output: "1A3B"
+Explanation: Bulls are connected with a '|' and cows are underlined:
+"1807"
+  |
+"7810"
 
+Example 2:
+Input: secret = "1123", guess = "0111"
+Output: "1A1B"
+Explanation: Bulls are connected with a '|' and cows are underlined:
+"1123"        "1123"
+  |      or     |
+"0111"        "0111"
+Note that only one of the two unmatched 1s is counted as a cow since the non-bull digits can only be rearranged to allow one 1 to be a bull.
 */
 
 public class Q299BullsAndCows {
