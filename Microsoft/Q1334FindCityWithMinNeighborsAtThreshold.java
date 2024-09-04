8506b888ebb6f01ed8749e472f1ae4e7a624d3c9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-04 19:09:16 +0530

    Added Q1334FindCityWithMinNeighborsAtThreshold code

--- Microsoft/Q1334FindCityWithMinNeighborsAtThreshold.java
+++ Microsoft/Q1334FindCityWithMinNeighborsAtThreshold.java
@@ -35,5 +35,50 @@ The city 0 has 1 neighboring city at a distanceThreshold = 2.
 */
 
 public class Q1334FindCityWithMinNeighborsAtThreshold {
-    
+
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int m = edges.length;
+        int[][] dist = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                dist[i][j] = Integer.MAX_VALUE;
+        }
+        for (int i = 0; i < m; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
+            dist[u][v] = wt;
+            dist[v][u] = wt;
+        }
+        // first make distance between same cities as 0
+        for (int i = 0; i < n; i++)
+            dist[i][i] = 0;
+
+        // Floyd Warshall Algorithm to find min distance b/w any two nodes
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] == Integer.MAX_VALUE ||
+                            dist[k][j] == Integer.MAX_VALUE)
+                        continue;
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        int cntCity = n;
+        int cityNo = -1;
+        for (int city = 0; city < n; city++) {
+            int cnt = 0;
+            for (int adjCity = 0; adjCity < n; adjCity++) {
+                if (dist[city][adjCity] <= distanceThreshold)
+                    cnt++;
+            }
+            if (cnt <= cntCity) {
+                cntCity = cnt;
+                cityNo = city;
+            }
+        }
+        return cityNo;
+    }
 }
