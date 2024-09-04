070ce6abf39306d498a5203f6bb71ba4d8931b06
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-04 10:03:44 +0530

    Added Q930BinarySubArraysWithSum code

--- Microsoft/Q930BinarySubArraysWithSum.java
+++ Microsoft/Q930BinarySubArraysWithSum.java
@@ -1,5 +1,13 @@
 package Microsoft;
 
+/*
+Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum 
+goal. A subarray is a contiguous part of the array.
+
+Example 1:
+
+*/
+
 public class Q930BinarySubArraysWithSum {
     
 }
