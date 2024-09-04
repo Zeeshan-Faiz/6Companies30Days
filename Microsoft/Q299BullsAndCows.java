dbc9ad2a4e5a9241baa803d87cfdc9de69a3a45a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-04 10:45:44 +0530

    Added Q299BullsAndCows code

--- Microsoft/Q299BullsAndCows.java
+++ Microsoft/Q299BullsAndCows.java
@@ -34,4 +34,31 @@ Note that only one of the two unmatched 1s is counted as a cow since the non-bul
 
 public class Q299BullsAndCows {
     
+    int bulls =0;
+        int cows =0;
+        int[] secretFreq =new int[10];
+
+        int[] guessFreq =new int[10];
+
+        // O(n)
+        for(int i=0;i<secret.length();i++){
+            char secretChar = secret.charAt(i);
+
+            char guessChar = guess.charAt(i);
+
+            if(secretChar == guessChar){
+                bulls++;
+            } else{
+                secretFreq[secretChar -'0']++;
+
+                guessFreq[guessChar -'0']++;
+            }
+        }
+
+        // O(1)
+        for(int i=0;i<10;i++){
+            cows += Math.min(secretFreq[i], guessFreq[i]);
+        }
+
+        return bulls +"A"+ cows +"B";
 }
