e4c72b57072103fc04c71a753c5689eb01a0da3e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-04 19:34:52 +0530

    Added Q2002MaxProductOfTwoPalindromicSubSeq code

--- Microsoft/Q2002MaxProductOfTwoPalindromicSubSeq.java
+++ Microsoft/Q2002MaxProductOfTwoPalindromicSubSeq.java
@@ -1,5 +1,18 @@
 package Microsoft;
 
+/*
+Given a string s, find two disjoint palindromic subsequences of s such that the product of their 
+lengths is maximized. The two subsequences are disjoint if they do not both pick a character at 
+the same index. Return the maximum possible product of the lengths of the two palindromic subsequences.
+
+A subsequence is a string that can be derived from another string by deleting some or no characters 
+without changing the order of the remaining characters. A string is palindromic if it reads the 
+same forward and backward.
+
+Example 1:
+
+*/
+
 public class Q2002MaxProductOfTwoPalindromicSubSeq {
     
 }
