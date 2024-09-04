40508f3c429fbc6f3b4ddec146025ab4e5677090
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-04 18:09:02 +0530

    Added Q187RepeatedDNA code

--- Microsoft/Q187RepeatedDNA.java
+++ Microsoft/Q187RepeatedDNA.java
@@ -17,5 +17,24 @@ Output: ["AAAAAAAAAA"]
 */
 
 public class Q187RepeatedDNA {
-    
+
+    public List<String> findRepeatedDnaSequences(String s) {
+
+        HashMap<String, Integer> freq = new HashMap<>();
+
+        // find all substrings of length 10 and add in map
+        // if the substring is already present in map then increase it's counter
+        for (int i = 0; i < s.length() - 9; i++) {
+            String dna = s.substring(i, i + 10);
+            freq.put(dna, freq.getOrDefault(dna, 0) + 1);
+        }
+
+        List<String> list = new ArrayList();
+        // add all the substrings which has counter > 1
+        for (Map.Entry<String, Integer> e : freq.entrySet()) {
+            if (e.getValue() > 1)
+                list.add(e.getKey());
+        }
+        return list;
+    }
 }
