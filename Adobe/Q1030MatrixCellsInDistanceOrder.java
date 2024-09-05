df0f640c50285fe6e2fcae8b7f7c974e5506f20f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-05 20:01:32 +0530

    Added Q1030MatrixCellsInDistanceOrder code

--- Adobe/Q1030MatrixCellsInDistanceOrder.java
+++ Adobe/Q1030MatrixCellsInDistanceOrder.java
@@ -27,5 +27,45 @@ There are other answers that would also be accepted as correct, such as [[1,2],[
 */
 
 public class Q1030MatrixCellsInDistanceOrder {
-    
+
+    public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        int[][] result = new int[rows * cols][];
+        result[0] = new int[] { rCenter, cCenter };
+        int resultIdx = 1;
+        int lim = Math.max(rCenter, rows - rCenter - 1) + Math.max(cCenter, cols - cCenter - 1);
+
+        for (int dist = 1; dist <= lim; dist++) {
+            int r = rCenter - dist;
+            int c = cCenter;
+
+            for (int count = dist; count > 0; count--) {
+                if (r >= 0 && c >= 0)
+                    result[resultIdx++] = new int[] { r, c };
+                r++;
+                c--;
+            }
+
+            for (int count = dist; count > 0; count--) {
+                if (r < rows && c >= 0)
+                    result[resultIdx++] = new int[] { r, c };
+                r++;
+                c++;
+            }
+
+            for (int count = dist; count > 0; count--) {
+                if (r < rows && c < cols)
+                    result[resultIdx++] = new int[] { r, c };
+                r--;
+                c++;
+            }
+
+            for (int count = dist; count > 0; count--) {
+                if (r >= 0 && c < cols)
+                    result[resultIdx++] = new int[] { r, c };
+                r--;
+                c--;
+            }
+        }
+        return result;
+    }
 }
