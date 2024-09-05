6888bb456a483428168b88c781cbe98a8ade11b6
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-05 10:06:20 +0530

    Added Q280WiggleSort code

--- Microsoft/Q280WiggleSort.java
+++ Microsoft/Q280WiggleSort.java
@@ -22,4 +22,11 @@ public class Q280WiggleSort {
                 swap(nums, i, i + 1);
         }
     }
+
+    private void swap(int[] nums, int a, int b) {
+        
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
 }
