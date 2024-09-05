3c99ca7db67c9b75e3dbf41fc61dd9a97aa7c9ac
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-05 19:42:35 +0530

    Added Q2745ConstructTheLongestString code

--- Adobe/Q2745ConstructTheLongestString.java
+++ Adobe/Q2745ConstructTheLongestString.java
@@ -1,5 +1,16 @@
 package Adobe;
 
+/*
+You are given three integers x, y, and z. You have x strings equal to "AA", y strings equal to "BB", 
+and z strings equal to "AB". You want to choose some (possibly all or none) of these strings and 
+concatenate them in some order to form a new string. This new string must not contain "AAA" or "BBB" 
+as a substring. Return the maximum possible length of the new string.
+A substring is a contiguous non-empty sequence of characters within a string.
+
+Example 1:
+
+*/
+
 public class Q2745ConstructTheLongestString {
     
 }
