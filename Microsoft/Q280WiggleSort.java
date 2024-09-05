8da27687de4bd06300261bb5e6be57817071732e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-05 10:03:20 +0530

    Added Q280WiggleSort code

--- Microsoft/Q280WiggleSort.java
+++ Microsoft/Q280WiggleSort.java
@@ -1,5 +1,13 @@
 package Microsoft;
 
+/*
+Given an array nums, reorder it "in-place" such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+
+Example 1:
+num = [3,5,2,1,6,4]
+O/P = [3,5,1,6,2,4]
+*/
+
 public class Q280WiggleSort {
     
 }
