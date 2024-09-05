a62d3f8f612998085bc024aefd46c6177dd16508
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-05 20:01:10 +0530

    Added Q1030MatrixCellsInDistanceOrder code

--- Adobe/Q1030MatrixCellsInDistanceOrder.java
+++ Adobe/Q1030MatrixCellsInDistanceOrder.java
@@ -1,5 +1,17 @@
 package Adobe;
 
+/*
+You are given four integers row, cols, rCenter, and cCenter. There is a rows x cols matrix and you 
+are on the cell with the coordinates (rCenter, cCenter).
+Return the coordinates of all cells in the matrix, sorted by their distance from (rCenter, cCenter) 
+from the smallest distance to the largest distance. You may return the answer in any order that 
+satisfies this condition.
+The distance between two cells (r1, c1) and (r2, c2) is |r1 - r2| + |c1 - c2|.
+
+Example 1:
+
+*/
+
 public class Q1030MatrixCellsInDistanceOrder {
     
 }
