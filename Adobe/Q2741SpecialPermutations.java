ba2840893f80c6fed6e7fa5b55ab4a00debda424
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-06 22:01:38 +0530

    Added Q2741SpecialPermutations code

--- Adobe/Q2741SpecialPermutations.java
+++ Adobe/Q2741SpecialPermutations.java
@@ -1,5 +1,15 @@
 package Adobe;
 
+/*
+You are given a 0-indexed integer array nums containing n distinct positive integers. A permutation 
+of nums is called special if:
+    For all indexes 0 <= i < n - 1, either nums[i] % nums[i+1] == 0 or nums[i+1] % nums[i] == 0.
+Return the total number of special permutations. As the answer could be large, return it modulo 109 + 7
+
+Example 1:
+
+*/
+
 public class Q2741SpecialPermutations {
     
 }
