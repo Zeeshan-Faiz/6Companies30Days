63db50ea902b2eafc91566e4bd4aa7e325ea6dd7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-07 11:14:18 +0530

    Added Q2225FindPlayersWithZeroOrOneLoss code

--- Adobe/Q2225FindPlayersWithZeroOrOneLoss.java
+++ Adobe/Q2225FindPlayersWithZeroOrOneLoss.java
@@ -1,5 +1,21 @@
 package Adobe;
 
+/*
+You are given an integer array matches where matches[i] = [winneri, loseri] indicates that the 
+player winneri defeated player loseri in a match.
+Return a list answer of size 2 where:
+    answer[0] is a list of all players that have not lost any matches.
+    answer[1] is a list of all players that have lost exactly one match.
+
+The values in the two lists should be returned in increasing order.
+Note:
+    You should only consider the players that have played at least one match.
+    The testcases will be generated such that no two matches will have the same outcome.
+
+Example 1:
+
+*/
+
 public class Q2225FindPlayersWithZeroOrOneLoss {
     
 }
