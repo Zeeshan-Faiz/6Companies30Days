ef0b1ccd2012dcdd4ad7db18273d8eb1280b13a8
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-08 18:46:39 +0530

    Added Q1386CinemaSeatAllocation code

--- Adobe/Q1386CinemaSeatAllocation.java
+++ Adobe/Q1386CinemaSeatAllocation.java
@@ -1,5 +1,10 @@
 package Adobe;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /*
 A cinema has n rows of seats, numbered from 1 to n and there are ten seats in each row, labelled 
 from 1 to 10 as shown in the figure above.
@@ -27,4 +32,44 @@ Output: 4
 
 public class Q1386CinemaSeatAllocation {
     
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int[] i : reservedSeats) {
+            map.putIfAbsent(i[0], new ArrayList<>());
+            map.get(i[0]).add(i[1]);
+        }
+
+        int ans = 2 * (n - map.size());
+
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            List<Integer> seats = entry.getValue();
+
+            boolean isLeftAisle = false, isRightAisle = false, isMiddle = false;
+
+            for (int seat : seats) {
+
+                if (seat >= 2 && seat <= 5)
+                    isLeftAisle = true;
+                if (seat >= 6 && seat <= 9)
+                    isRightAisle = true;
+                if (seat >= 4 && seat <= 7)
+                    isMiddle = true;
+
+                if (isLeftAisle && isRightAisle && isMiddle) {
+                    break;
+                }
+            }
+
+            if (!isLeftAisle)
+                ans += 1;
+            if (!isRightAisle)
+                ans += 1;
+            if (isLeftAisle && isRightAisle && !isMiddle)
+                ans += 1;
+
+        }
+
+        return ans;
+    }
 }
