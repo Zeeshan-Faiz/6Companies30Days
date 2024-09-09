c80ea65325f990678afc4b260ee0716698af75e5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-09 10:32:34 +0530

    Added Q1208GetEqualSubstrings code

--- Adobe/Q1208GetEqualSubstrings.java
+++ Adobe/Q1208GetEqualSubstrings.java
@@ -1,5 +1,16 @@
 package Adobe;
 
+/*
+You want to change s to t. Changing the ith character of s to ith character of t costs 
+|s[i] - t[i]| (i.e., the absolute difference between the ASCII values of the characters).
+Return the maximum length of a substring of s that can be changed to be the same as the 
+corresponding substring of t with a cost less than or equal to maxCost. If there is no substring 
+from s that can be changed to its corresponding substring from t, return 0.
+
+Example 1:
+
+*/
+
 public class Q1208GetEqualSubstrings{
 
 }
