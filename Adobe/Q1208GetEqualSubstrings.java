9d6cc92bce7e1e2a06a4ec0e1ba56dc7cc5511f0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-09 10:32:48 +0530

    Added Q1208GetEqualSubstrings code

--- Adobe/Q1208GetEqualSubstrings.java
+++ Adobe/Q1208GetEqualSubstrings.java
@@ -8,7 +8,20 @@ corresponding substring of t with a cost less than or equal to maxCost. If there
 from s that can be changed to its corresponding substring from t, return 0.
 
 Example 1:
+Input: s = "abcd", t = "bcdf", maxCost = 3
+Output: 3
+Explanation: "abc" of s can change to "bcd".
+That costs 3, so the maximum length is 3.
 
+Example 2:
+Input: s = "abcd", t = "cdef", maxCost = 3
+Output: 1
+Explanation: Each character in s costs 2 to change to character in t,  so the maximum length is 1.
+
+Example 3:
+Input: s = "abcd", t = "acde", maxCost = 0
+Output: 1
+Explanation: You cannot make any change, so the maximum length is 1.
 */
 
 public class Q1208GetEqualSubstrings{
