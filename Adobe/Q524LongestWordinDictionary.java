dcd27c5f42a2a0ef68e9b341994df775298b0b17
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-10 10:51:47 +0530

    Added Q524LongestWordinDictionary code

--- Adobe/Q524LongestWordinDictionary.java
+++ Adobe/Q524LongestWordinDictionary.java
@@ -1,5 +1,9 @@
 package Adobe;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /*
 Given a string s and a string array dictionary, return the longest string in the dictionary that 
 can be formed by deleting some of the given string characters. If there is more than one possible 
@@ -16,5 +20,32 @@ Output: "a"
 */
 
 public class Q524LongestWordinDictionary {
-    
+
+    public String findLongestWord(String s, List<String> dictionary) {
+
+        // Sorting the dictionary in reverse Order...
+        Collections.sort(dictionary, Comparator.reverseOrder());
+        String longest = "";
+        for (String word : dictionary) {
+            // If word can be formed and the current longest is less than or equal in length...
+            if (WordCanBeFormed(s, word) && longest.length() <= word.length())
+                longest = word;
+        }
+        return longest;
+    }
+
+    public boolean WordCanBeFormed(String sequence, String word) {
+
+        StringBuilder greedy = new StringBuilder();
+
+        for (int i = 0, j = 0; i < sequence.length(); i++) {
+            // If the characters are same at both indices
+            if (j != word.length() && sequence.charAt(i) == word.charAt(j)) {
+                greedy.append("" + sequence.charAt(i));
+                j++;
+            }
+        }
+        // Checking if the word can be developed as a subsequence
+        return greedy.toString().equals(word);
+    }
 }
