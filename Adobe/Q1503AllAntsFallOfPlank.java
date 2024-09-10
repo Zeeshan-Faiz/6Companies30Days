42e6add7a83c2122add4d238b2233cc04815ffd2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-10 10:42:15 +0530

    Added Q1503AllAntsFallOfPlank code

--- Adobe/Q1503AllAntsFallOfPlank.java
+++ Adobe/Q1503AllAntsFallOfPlank.java
@@ -1,5 +1,18 @@
 package Adobe;
 
+/*
+We have a wooden plank of the length n units. Some ants are walking on the plank, each ant moves 
+with a speed of 1 unit per second. Some of the ants move to the left, the other move to the right.
+When two ants moving in two different directions meet at some point, they change their directions 
+and continue moving again. Assume changing directions does not take any additional time.
+When an ant reaches one end of the plank at a time t, it falls out of the plank immediately.
+Given an integer n and two integer arrays left and right, the positions of the ants moving to 
+left and the right, return the moment when the last ant(s) fall out of the plank.
+
+Example 1:
+
+*/
+
 public class Q1503AllAntsFallOfPlank {
     
 }
