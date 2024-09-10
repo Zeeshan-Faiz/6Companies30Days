3e202f7f4a4d12826e7a3b948e89c35053b7431a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-10 10:50:16 +0530

    Added Q524LongestWordinDictionary code

--- Adobe/Q524LongestWordinDictionary.java
+++ Adobe/Q524LongestWordinDictionary.java
@@ -1,5 +1,15 @@
 package Adobe;
 
+/*
+Given a string s and a string array dictionary, return the longest string in the dictionary that 
+can be formed by deleting some of the given string characters. If there is more than one possible 
+result, return the longest word with the smallest lexicographical order. If there is no possible 
+result, return the empty string.
+
+Example 1:
+
+*/
+
 public class Q524LongestWordinDictionary {
     
 }
