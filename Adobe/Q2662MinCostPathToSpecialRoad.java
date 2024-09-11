a59dd157088401eda29447b1b539078af0897c0e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-11 10:17:30 +0530

    Added Q2662MinCostPathToSpecialRoad code

--- Adobe/Q2662MinCostPathToSpecialRoad.java
+++ Adobe/Q2662MinCostPathToSpecialRoad.java
@@ -1,5 +1,20 @@
 package Adobe;
 
+/*
+You are given an array start where start = [startX, startY] represents your initial position 
+(startX, startY) in a 2D space. You are also given the array target where target = [targetX, 
+targetY] represents your target position (targetX, targetY).
+The cost of going from a position (x1, y1) to any other position in the space (x2, y2) is 
+|x2 - x1| + |y2 - y1|. There are also some special roads. You are given a 2D array specialRoads 
+where specialRoads[i] = [x1i, y1i, x2i, y2i, costi] indicates that the ith special road goes in 
+one direction from (x1i, y1i) to (x2i, y2i) with a cost equal to costi. You can use each special 
+road any number of times.
+Return the minimum cost required to go from (startX, startY) to (targetX, targetY).
+
+Example 1:
+
+*/
+
 public class Q2662MinCostPathToSpecialRoad {
     
 }
