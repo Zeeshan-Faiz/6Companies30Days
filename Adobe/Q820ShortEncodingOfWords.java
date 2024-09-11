68ebd6124cf6d028f395a98dc0e1b11ec27fc4fa
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-11 10:12:45 +0530

    Added Q820ShortEncodingOfWords code

--- Adobe/Q820ShortEncodingOfWords.java
+++ Adobe/Q820ShortEncodingOfWords.java
@@ -11,7 +11,17 @@ Given an array of words, return the length of the shortest reference string s po
 encoding of words.
 
 Example 1:
+Input: words = ["time", "me", "bell"]
+Output: 10
+Explanation: A valid encoding would be s = "time#bell#" and indices = [0, 2, 5].
+words[0] = "time", the substring of s starting from indices[0] = 0 to the next '#' is underlined in "time#bell#"
+words[1] = "me", the substring of s starting from indices[1] = 2 to the next '#' is underlined in "time#bell#"
+words[2] = "bell", the substring of s starting from indices[2] = 5 to the next '#' is underlined in "time#bell#"
 
+Example 2:
+Input: words = ["t"]
+Output: 2
+Explanation: A valid encoding would be s = "t#" and indices = [0]
 */
 
 public class Q820ShortEncodingOfWords {
