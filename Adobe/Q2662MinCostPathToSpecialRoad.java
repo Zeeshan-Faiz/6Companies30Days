7e0afdfbcabe964d9e210bf036bdb7f3e22974c9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-11 10:18:06 +0530

    Added Q2662MinCostPathToSpecialRoad code

--- Adobe/Q2662MinCostPathToSpecialRoad.java
+++ Adobe/Q2662MinCostPathToSpecialRoad.java
@@ -12,7 +12,29 @@ road any number of times.
 Return the minimum cost required to go from (startX, startY) to (targetX, targetY).
 
 Example 1:
+Input: start = [1,1], target = [4,5], specialRoads = [[1,2,3,3,2],[3,4,4,5,1]]
+Output: 5
+Explanation:
+    (1,1) to (1,2) with a cost of |1 - 1| + |2 - 1| = 1.
+    (1,2) to (3,3). Use specialRoads[0] with the cost 2.
+    (3,3) to (3,4) with a cost of |3 - 3| + |4 - 3| = 1.
+    (3,4) to (4,5). Use specialRoads[1] with the cost 1.
+So the total cost is 1 + 2 + 1 + 1 = 5.
 
+Example 2:
+Input: start = [3,2], target = [5,7], specialRoads = [[5,7,3,2,1],[3,2,3,4,4],[3,3,5,5,5],[3,4,5,6,6]]
+Output: 7
+Explanation:
+It is optimal not to use any special edges and go directly from the starting to the ending position with a cost |5 - 3| + |7 - 2| = 7.
+Note that the specialRoads[0] is directed from (5,7) to (3,2).
+
+Example 3:
+Input: start = [1,1], target = [10,4], specialRoads = [[4,2,1,1,3],[1,2,7,4,4],[10,3,6,1,2],[6,1,1,2,3]]
+Output: 8
+Explanation:
+    (1,1) to (1,2) with a cost of |1 - 1| + |2 - 1| = 1.
+    (1,2) to (7,4). Use specialRoads[1] with the cost 4.
+    (7,4) to (10,4) with a cost of |10 - 7| + |4 - 4| = 3.
 */
 
 public class Q2662MinCostPathToSpecialRoad {
