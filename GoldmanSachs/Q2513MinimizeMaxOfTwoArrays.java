708a50af8ca3a8a8fa82ff1c6d6e2b6ffb27ea9f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-12 10:36:10 +0530

    Added Q2513MinimizeMaxOfTwoArrays code

--- GoldmanSachs/Q2513MinimizeMaxOfTwoArrays.java
+++ GoldmanSachs/Q2513MinimizeMaxOfTwoArrays.java
@@ -36,22 +36,24 @@ It can be shown that it is not possible to obtain a lower maximum satisfying all
 public class Q2513MinimizeMaxOfTwoArrays {
 
     public int minimizeSet(int divisor1, int divisor2, int uniqueCnt1, int uniqueCnt2) {
+        
         long low = 1;
         long high = (long) Math.pow(10, 10);
         long count1 = 0;
         long count2 = 0;
         long common = 0;
 
-        while (low < high) {
+        while (low < high) 
+        {
             long mid = (low + high) / 2;
             count1 = mid - (mid / divisor1);
             count2 = mid - (mid / divisor2);
             common = mid - (mid / lcm(divisor1, divisor2));
             if (count1 >= uniqueCnt1 && count2 >= uniqueCnt2 && common >= uniqueCnt1 + uniqueCnt2) {
                 high = mid;
-            } else {
+            } 
+            else
                 low = mid + 1;
-            }
         }
         return (int) low;
     }
