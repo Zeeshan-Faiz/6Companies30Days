0d252528c014dfd0ace35d5a8b365f97011bab2b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-12 10:35:14 +0530

    Added Q2513MinimizeMaxOfTwoArrays code

--- GoldmanSachs/Q2513MinimizeMaxOfTwoArrays.java
+++ GoldmanSachs/Q2513MinimizeMaxOfTwoArrays.java
@@ -1,5 +1,18 @@
 package GoldmanSachs;
 
+/*
+We have two arrays arr1 and arr2 which are initially empty. You need to add positive integers to 
+them such that they satisfy all the following conditions:
+    arr1 contains uniqueCnt1 distinct positive integers, each of which is not divisible by divisor1.
+    arr2 contains uniqueCnt2 distinct positive integers, each of which is not divisible by divisor2.
+    No integer is present in both arr1 and arr2.
+Given divisor1, divisor2, uniqueCnt1, and uniqueCnt2, return the minimum possible maximum integer 
+that can be present in either array.
+
+Example 1:
+
+*/
+
 public class Q2513MinimizeMaxOfTwoArrays {
     
 }
