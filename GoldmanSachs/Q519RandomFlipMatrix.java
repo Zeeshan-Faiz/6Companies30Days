eb78cfa4c40a8e8e176d6fb8e62cdbec0232bcd9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-14 20:04:46 +0530

    Added Q519RandomFlipMatrix code

--- GoldmanSachs/Q519RandomFlipMatrix.java
+++ GoldmanSachs/Q519RandomFlipMatrix.java
@@ -1,5 +1,21 @@
 package GoldmanSachs;
 
+/*
+There is an m x n binary grid matrix with all the values set 0 initially. Design an algorithm to 
+randomly pick an index (i, j) where matrix[i][j] == 0 and flips it to 1. All the indices (i, j) 
+where matrix[i][j] == 0 should be equally likely to be returned.
+Optimize your algorithm to minimize the number of calls made to the built-in random function of 
+your language and optimize the time and space complexity.
+Implement the Solution class:
+
+    Solution(int m, int n) Initializes the object with the size of the binary matrix m and n.
+    int[] flip() Returns a random index [i, j] of the matrix where matrix[i][j] == 0 and flips it to 1.
+    void reset() Resets all the values of the matrix to be 0.
+
+Example 1:
+
+*/
+
 public class Q519RandomFlipMatrix {
     
 }
