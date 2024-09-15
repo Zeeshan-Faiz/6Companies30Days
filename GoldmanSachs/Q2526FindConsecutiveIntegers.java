1de39370608c8120380ecae5421638677eaa98c0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-15 19:30:29 +0530

    Added Q2526FindConsecutiveIntegers code

--- GoldmanSachs/Q2526FindConsecutiveIntegers.java
+++ GoldmanSachs/Q2526FindConsecutiveIntegers.java
@@ -1,5 +1,19 @@
 package GoldmanSachs;
 
+/*
+For a stream of integers, implement a data structure that checks if the last k integers parsed in 
+the stream are equal to value.
+Implement the DataStream class:
+    DataStream(int value, int k) Initializes the object with an empty integer stream and the two 
+    integers value and k.
+    boolean consec(int num) Adds num to the stream of integers. Returns true if the last k 
+    integers are equal to value, and false otherwise. If there are less than k integers, the 
+    condition does not hold true, so returns false.
+
+Example 1:
+
+*/
+
 public class Q2526FindConsecutiveIntegers {
     
 }
