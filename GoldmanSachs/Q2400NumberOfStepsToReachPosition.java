3863d17fb9d24da3422db71a1d4be6c59b256717
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-17 20:26:16 +0530

    Added Q2400NumberOfStepsToReachPosition code

--- GoldmanSachs/Q2400NumberOfStepsToReachPosition.java
+++ GoldmanSachs/Q2400NumberOfStepsToReachPosition.java
@@ -1,5 +1,16 @@
 package GoldmanSachs;
 
+/*
+You are given two positive integers startPos and endPos. Initially, you are standing at position 
+startPos on an infinite number line. With one step, you can move either one position to the left,
+or one position to the right.
+Given a positive integer k, return the number of different ways to reach the position endPos 
+starting from startPos, such that you perform exactly k steps. Since the answer may be very 
+large, return it modulo 109 + 7. Two ways are considered different if the order of the steps made 
+is not exactly the same.
+Note that the number line includes negative integers.
+*/
+
 public class Q2400NumberOfStepsToReachPosition {
     
 }
