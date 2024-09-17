f3182faa6f95055159f716a6031cd93369f4af2d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-17 20:40:37 +0530

    Added Q368LargestDivisibleSubset code

--- Walmart/Q368LargestDivisibleSubset.java
+++ Walmart/Q368LargestDivisibleSubset.java
@@ -1,5 +1,9 @@
 package Walmart;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
 Given a set of distinct positive integers nums, return the largest subset answer such that 
 every pair (answer[i], answer[j]) of elements in this subset satisfies:
@@ -19,4 +23,35 @@ Output: [1,2,4,8]
 
 public class Q368LargestDivisibleSubset {
     
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,-1);
+        Arrays.sort(nums);
+        int[] hash = new int[nums.length];
+        int maxi = -1;
+        int lastIndex = 0;
+        for(int index=0;index<nums.length;index++){
+            hash[index] = index;
+            for(int col=0;col<index;col++){
+                
+                if(nums[index]%nums[col]==0 && 1 + dp[col] > dp[index]){
+                    dp[index] = 1 + dp[col];
+                    hash[index] = col;
+                }
+            }
+            if(dp[index]>maxi){
+                maxi = dp[index];
+                lastIndex = index;
+            }
+        } 
+        List<Integer> lst = new ArrayList<Integer>();
+        lst.add(nums[lastIndex]);
+        while(hash[lastIndex]!=lastIndex){
+            lastIndex = hash[lastIndex];
+            lst.add(nums[lastIndex]);
+            
+
+        }
+        return lst;
+    }
 }
