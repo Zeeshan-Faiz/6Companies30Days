16aecf3be5823ee33fc84539603201d2f303f1c7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-17 20:40:09 +0530

    Added Q368LargestDivisibleSubset code

--- Walmart/Q368LargestDivisibleSubset.java
+++ Walmart/Q368LargestDivisibleSubset.java
@@ -1,5 +1,16 @@
 package Walmart;
 
+/*
+Given a set of distinct positive integers nums, return the largest subset answer such that 
+every pair (answer[i], answer[j]) of elements in this subset satisfies:
+    answer[i] % answer[j] == 0, or
+    answer[j] % answer[i] == 0
+
+If there are multiple solutions, return any of them.
+Example 1:
+
+*/
+
 public class Q368LargestDivisibleSubset {
     
 }
