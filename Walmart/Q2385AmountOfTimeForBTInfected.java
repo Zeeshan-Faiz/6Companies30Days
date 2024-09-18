9a2cdf1a8df99b97f6e37e55500316b2a751910f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-18 22:03:42 +0530

    Added Q2385AmountOfTimeForBTInfected code

--- Walmart/Q2385AmountOfTimeForBTInfected.java
+++ Walmart/Q2385AmountOfTimeForBTInfected.java
@@ -10,7 +10,20 @@ Each minute, a node becomes infected if:
 Return the number of minutes needed for the entire tree to be infected.
 
 Example 1:
+Input: root = [1,5,3,null,4,10,6,9,2], start = 3
+Output: 4
+Explanation: The following nodes are infected during:
+- Minute 0: Node 3
+- Minute 1: Nodes 1, 10 and 6
+- Minute 2: Node 5
+- Minute 3: Node 4
+- Minute 4: Nodes 9 and 2
+It takes 4 minutes for the whole tree to be infected so we return 4.
 
+Example 2:
+Input: root = [1], start = 1
+Output: 0
+Explanation: At minute 0, the only node in the tree is infected so we return 0.
 */
 
 public class Q2385AmountOfTimeForBTInfected {
