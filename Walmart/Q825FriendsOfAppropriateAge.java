bf67cc726b1ef61273155cf4b6d715987c44e06d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-20 10:50:06 +0530

    Added Q825FriendsOfAppropriateAge code

--- Walmart/Q825FriendsOfAppropriateAge.java
+++ Walmart/Q825FriendsOfAppropriateAge.java
@@ -14,7 +14,19 @@ necessarily send a request to x. Also, a person will not send a friend request t
 Return the total number of friend requests made.
 
 Example 1:
+Input: ages = [16,16]
+Output: 2
+Explanation: 2 people friend request each other.
 
+Example 2:
+Input: ages = [16,17,18]
+Output: 2
+Explanation: Friend requests are made 17 -> 16, 18 -> 17.
+
+Example 3:
+Input: ages = [20,30,100,110,120]
+Output: 3
+Explanation: Friend requests are made 110 -> 100, 120 -> 110, 120 -> 100.
 */
 
 public class Q825FriendsOfAppropriateAge {
