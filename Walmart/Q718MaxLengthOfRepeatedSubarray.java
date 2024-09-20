677ad29990bdf69c7295b32dbc512e8ee54469a2
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-20 10:52:39 +0530

    Added Q718MaxLengthOfRepeatedSubarray code

--- Walmart/Q718MaxLengthOfRepeatedSubarray.java
+++ Walmart/Q718MaxLengthOfRepeatedSubarray.java
@@ -16,5 +16,24 @@ Explanation: The repeated subarray with maximum length is [0,0,0,0,0].
 */
 
 public class Q718MaxLengthOfRepeatedSubarray {
-    
+ 
+    public int findLength(int[] nums1, int[] nums2) {
+
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        int max = 0;
+
+        // Iterate over each position in the matrix
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+
+                // If numbers are equal
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    // Get the number from diagonally opposite and add 1
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max = Math.max(dp[i][j], max);
+                }
+            }
+        }
+        return max;
+    }
 }
