b5e7c3b7c0f0d66f7fac8ae1142e31df8f7c7c23
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-20 10:52:27 +0530

    Added Q718MaxLengthOfRepeatedSubarray code

--- Walmart/Q718MaxLengthOfRepeatedSubarray.java
+++ Walmart/Q718MaxLengthOfRepeatedSubarray.java
@@ -5,7 +5,14 @@ Given two integer arrays nums1 and nums2, return the maximum length of a subarra
 in both arrays.
 
 Example 1:
+Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+Output: 3
+Explanation: The repeated subarray with maximum length is [3,2,1].
 
+Example 2:
+Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+Output: 5
+Explanation: The repeated subarray with maximum length is [0,0,0,0,0].
 */
 
 public class Q718MaxLengthOfRepeatedSubarray {
