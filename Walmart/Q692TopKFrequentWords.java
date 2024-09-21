0d3358e952599b2a3471f94396c594befc226542
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-21 17:04:04 +0530

    Added Q692TopKFrequentWords code

--- Walmart/Q692TopKFrequentWords.java
+++ Walmart/Q692TopKFrequentWords.java
@@ -1,5 +1,14 @@
 package Walmart;
 
+/*
+Given an array of strings words and an integer k, return the k most frequent strings.
+Return the answer sorted by the frequency from highest to lowest. Sort the words with the same 
+frequency by their lexicographical order.
+
+Example 1:
+
+*/
+
 public class Q692TopKFrequentWords {
     
 }
