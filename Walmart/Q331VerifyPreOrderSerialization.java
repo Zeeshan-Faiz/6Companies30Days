b10dad194b83ba86bc9000545ace96de101d415a
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-21 17:00:44 +0530

    Added Q331VerifyPreOrderSerialization code

--- Walmart/Q331VerifyPreOrderSerialization.java
+++ Walmart/Q331VerifyPreOrderSerialization.java
@@ -28,5 +28,20 @@ Output: false
 */
 
 public class Q331VerifyPreOrderSerialization {
-    
+
+    public boolean isValidSerialization(String preorder) {
+        
+        int balance = 1;
+        String[] nodes = preorder.split(",");
+
+        for (String node : nodes) {
+            if (balance <= 0)
+                return false;
+            if (node.equals("#"))
+                balance--;
+            else
+                balance++;
+        }
+        return balance == 0;
+    }
 }
