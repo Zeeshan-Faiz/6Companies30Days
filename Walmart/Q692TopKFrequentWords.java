63a65554d3b04630c56859348987e6ce9e2bc450
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-21 17:04:52 +0530

    Added Q692TopKFrequentWords code

--- Walmart/Q692TopKFrequentWords.java
+++ Walmart/Q692TopKFrequentWords.java
@@ -1,5 +1,13 @@
 package Walmart;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 /*
 Given an array of strings words and an integer k, return the k most frequent strings.
 Return the answer sorted by the frequency from highest to lowest. Sort the words with the same 
@@ -18,5 +26,41 @@ Explanation: "the", "is", "sunny" and "day" are the four most frequent words, wi
 */
 
 public class Q692TopKFrequentWords {
-    
+
+    public List<String> topKFrequent(String[] words, int k) {
+
+        Map<String, Integer> map = getFreq(words);
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(new MyComparator());
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            pq.offer(e);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        List<String> res = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            res.add(pq.poll().getKey());
+        }
+        Collections.reverse(res);
+        return res;
+    }
+
+    private Map<String, Integer> getFreq(String[] words) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : words) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        return map;
+    }
+
+    static class MyComparator implements Comparator<Map.Entry<String, Integer>> {
+        @Override
+        public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) {
+            int res = Integer.compare(e1.getValue(), e2.getValue());
+            if (res == 0) {
+                return e2.getKey().compareTo(e1.getKey());
+            }
+            return res;
+        }
+    }
 }
