17dbbf1b57ac9a9ecd0428c1c36e518e07b32832
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-22 11:23:09 +0530

    Added Q419BattleShipsInBoard code

--- Walmart/Q419BattleShipsInBoard.java
+++ Walmart/Q419BattleShipsInBoard.java
@@ -18,5 +18,39 @@ Output: 0
 */
 
 public class Q419BattleShipsInBoard {
-    
+
+    public int countBattleships(char[][] board) {
+        
+        int nr = board.length;
+        int nc = board[0].length;
+        int count = 0;
+        for (int i = 0; i < nr; i++) {
+            for (int j = 0; j < nc; j++) {
+                if (board[i][j] == 'X') {
+                    count++;
+                    dfs(board, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private static void dfs(char[][] board, int row, int col) {
+        
+        int nr = board.length;
+        int nc = board[0].length;
+        board[row][col] = '.';
+        if ((row - 1) >= 0 && board[row - 1][col] == 'X') {
+            dfs(board, row - 1, col);
+        }
+        if ((row + 1) < nr && board[row + 1][col] == 'X') {
+            dfs(board, row + 1, col);
+        }
+        if (((col - 1) >= 0) && board[row][col - 1] == 'X') {
+            dfs(board, row, col - 1);
+        }
+        if ((col + 1) < nc && board[row][col + 1] == 'X') {
+            dfs(board, row, col + 1);
+        }
+    }
 }
