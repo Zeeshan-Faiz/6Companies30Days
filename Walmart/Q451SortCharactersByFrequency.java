8dd29249ee3298fcfeb0bb9caf591a316143a7cb
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-22 11:24:19 +0530

    Added Q451SortCharactersByFrequency code

--- Walmart/Q451SortCharactersByFrequency.java
+++ Walmart/Q451SortCharactersByFrequency.java
@@ -1,5 +1,14 @@
 package Walmart;
 
+/*
+Given a string s, sort it in decreasing order based on the frequency of the characters. The 
+frequency of a character is the number of times it appears in the string.
+Return the sorted string. If there are multiple answers, return any of them.
+
+Example 1:
+
+*/
+
 public class Q451SortCharactersByFrequency {
     
 }
