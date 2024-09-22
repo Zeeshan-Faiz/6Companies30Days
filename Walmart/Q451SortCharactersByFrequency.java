8628b5c6c519444b1a904b67c6dfaee75031f249
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-22 11:24:31 +0530

    Added Q451SortCharactersByFrequency code

--- Walmart/Q451SortCharactersByFrequency.java
+++ Walmart/Q451SortCharactersByFrequency.java
@@ -6,7 +6,22 @@ frequency of a character is the number of times it appears in the string.
 Return the sorted string. If there are multiple answers, return any of them.
 
 Example 1:
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
 
+Example 2:
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+Note that "cacaca" is incorrect, as the same characters must be together.
+
+Example 3:
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
 */
 
 public class Q451SortCharactersByFrequency {
