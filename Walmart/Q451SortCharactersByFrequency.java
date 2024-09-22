637d9a3f70854e39a263aaa38b24951e39d18e53
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-22 11:24:42 +0530

    Added Q451SortCharactersByFrequency code

--- Walmart/Q451SortCharactersByFrequency.java
+++ Walmart/Q451SortCharactersByFrequency.java
@@ -1,5 +1,7 @@
 package Walmart;
 
+import java.util.Arrays;
+
 /*
 Given a string s, sort it in decreasing order based on the frequency of the characters. The 
 frequency of a character is the number of times it appears in the string.
@@ -25,5 +27,24 @@ Note that 'A' and 'a' are treated as two different characters.
 */
 
 public class Q451SortCharactersByFrequency {
-    
+
+    public String frequencySort(String s) {
+
+        int[][] freq = new int[75][2];
+        for (int i = 0; i < s.length(); i++) {
+            int x = s.charAt(i) - '0';
+            freq[x][0]++;
+            freq[x][1] = x;
+        }
+        Arrays.sort(freq, (a, b) -> b[0] - a[0]);
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < freq.length; i++) {
+            int x = freq[i][1] + '0';
+            while (freq[i][0] > 0) {
+                result.append((char) x);
+                freq[i][0]--;
+            }
+        }
+        return result.toString();
+    }
 }
