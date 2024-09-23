cf59f564f6bb2aa1a4e01199c6d48ed1f88ee15e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-23 21:53:50 +0530

    Added Q703KthLargestElement code

--- Atlassian/Q703KthLargestElement.java
+++ Atlassian/Q703KthLargestElement.java
@@ -13,7 +13,29 @@ Implement the KthLargest class:
     int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far.
 
 Example 1:
+Input:
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output: [null, 4, 5, 5, 8, 8]
+Explanation:
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3); // return 4
+kthLargest.add(5); // return 5
+kthLargest.add(10); // return 5
+kthLargest.add(9); // return 8
+kthLargest.add(4); // return 8
 
+Example 2:
+Input:
+["KthLargest", "add", "add", "add", "add"]
+[[4, [7, 7, 7, 7, 8, 3]], [2], [10], [9], [9]]
+Output: [null, 7, 7, 7, 8]
+Explanation:
+KthLargest kthLargest = new KthLargest(4, [7, 7, 7, 7, 8, 3]);
+kthLargest.add(2); // return 7
+kthLargest.add(10); // return 7
+kthLargest.add(9); // return 7
+kthLargest.add(9); // return 8
 */
 
 public class Q703KthLargestElement {
