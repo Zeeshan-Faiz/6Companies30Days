52a48eb316e9cf2a47e64142508f4eddecd94807
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-23 21:53:18 +0530

    Added Q703KthLargestElement code

--- Atlassian/Q703KthLargestElement.java
+++ Atlassian/Q703KthLargestElement.java
@@ -1,5 +1,21 @@
 package Atlassian;
 
+/*
+You are part of a university admissions office and need to keep track of the kth highest test 
+score from applicants in real-time. This helps to determine cut-off marks for interviews and 
+admissions dynamically as new applicants submit their scores.
+You are tasked to implement a class which, for a given integer k, maintains a stream of test 
+scores and continuously returns the kth highest test score after a new score has been submitted. 
+More specifically, we are looking for the kth highest score in the sorted list of all scores.
+Implement the KthLargest class:
+
+    KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+    int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far.
+
+Example 1:
+
+*/
+
 public class Q703KthLargestElement {
     
 }
