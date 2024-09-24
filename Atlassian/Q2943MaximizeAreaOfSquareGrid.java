77e5861d0ffe283f7801d91bef422c2ee68c9ea0
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-24 22:14:39 +0530

    Added Q2943MaximizeAreaOfSquareGrid code

--- Atlassian/Q2943MaximizeAreaOfSquareGrid.java
+++ Atlassian/Q2943MaximizeAreaOfSquareGrid.java
@@ -1,5 +1,7 @@
 package Atlassian;
 
+import java.util.Arrays;
+
 /*
 You are given the two integers, n and m and two integer arrays, hBars and vBars. The grid has 
 n + 2 horizontal and m + 2 vertical bars, creating 1 x 1 unit cells. The bars are indexed 
@@ -30,5 +32,28 @@ One way to get the maximum square-shaped hole is by removing horizontal bar 3, a
 */
 
 public class Q2943MaximizeAreaOfSquareGrid {
-    
+
+    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+        int x = find(hBars), y = find(vBars);
+        int res = Math.min(x, y) + 1;
+
+        return res * res;
+    }
+
+    private int find(int[] arr) {
+        
+        Arrays.sort(arr);
+        int res = 1, i = 0, n = arr.length;
+        while (i < n) 
+        {
+            int count = 1;
+            while (i + 1 < n && arr[i] + 1 == arr[i + 1]) {
+                i++;
+                count++;
+            }
+            i++;
+            res = Math.max(res, count);
+        }
+        return res;
+    }
 }
