4f2b6126f08088bd19f2b921cd1f4df4e2020c0f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-24 22:04:02 +0530

    Added Q2943MaximizeAreaOfSquareGrid code

--- Atlassian/Q2943MaximizeAreaOfSquareGrid.java
+++ Atlassian/Q2943MaximizeAreaOfSquareGrid.java
@@ -1,5 +1,18 @@
 package Atlassian;
 
+/*
+You are given the two integers, n and m and two integer arrays, hBars and vBars. The grid has 
+n + 2 horizontal and m + 2 vertical bars, creating 1 x 1 unit cells. The bars are indexed 
+starting from 1.
+You can remove some of the bars in hBars from horizontal bars and some of the bars in vBars from 
+vertical bars. Note that other bars are fixed and cannot be removed.
+Return an integer denoting the maximum area of a square-shaped hole in the grid, after removing 
+some bars (possibly none)
+
+Example 1:
+
+*/
+
 public class Q2943MaximizeAreaOfSquareGrid {
     
 }
