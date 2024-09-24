e68452dfe653789658b6aa2ba051b549ec7eb090
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-24 22:01:42 +0530

    Added Q1600ThroneInheritance code

--- Atlassian/Q1600ThroneInheritance.java
+++ Atlassian/Q1600ThroneInheritance.java
@@ -29,7 +29,23 @@ Implement the ThroneInheritance class:
     string[] getInheritanceOrder() Returns a list representing the current order of inheritance excluding dead people.
 
 Example 1:
+Input
+["ThroneInheritance", "birth", "birth", "birth", "birth", "birth", "birth", "getInheritanceOrder", "death", "getInheritanceOrder"]
+[["king"], ["king", "andy"], ["king", "bob"], ["king", "catherine"], ["andy", "matthew"], ["bob", "alex"], ["bob", "asha"], [null], ["bob"], [null]]
+Output
+[null, null, null, null, null, null, null, ["king", "andy", "matthew", "bob", "alex", "asha", "catherine"], null, ["king", "andy", "matthew", "alex", "asha", "catherine"]]
 
+Explanation
+ThroneInheritance t= new ThroneInheritance("king"); // order: king
+t.birth("king", "andy"); // order: king > andy
+t.birth("king", "bob"); // order: king > andy > bob
+t.birth("king", "catherine"); // order: king > andy > bob > catherine
+t.birth("andy", "matthew"); // order: king > andy > matthew > bob > catherine
+t.birth("bob", "alex"); // order: king > andy > matthew > bob > alex > catherine
+t.birth("bob", "asha"); // order: king > andy > matthew > bob > alex > asha > catherine
+t.getInheritanceOrder(); // return ["king", "andy", "matthew", "bob", "alex", "asha", "catherine"]
+t.death("bob"); // order: king > andy > matthew > bob > alex > asha > catherine
+t.getInheritanceOrder(); // return ["king", "andy", "matthew", "alex", "asha", "catherine"]
 */
 
 public class Q1600ThroneInheritance {
