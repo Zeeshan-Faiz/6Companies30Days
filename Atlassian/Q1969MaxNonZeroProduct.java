3c3e2077a14f7926e35b0e42355d54f9e3199107
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-27 22:02:30 +0530

    Added Q1969MaxNonZeroProduct code

--- Atlassian/Q1969MaxNonZeroProduct.java
+++ Atlassian/Q1969MaxNonZeroProduct.java
@@ -37,5 +37,34 @@ The array product is 1 * 6 * 1 * 6 * 1 * 6 * 7 = 1512, which is the minimum poss
 */
 
 public class Q1969MaxNonZeroProduct {
-    
+
+    public int mod = 1_000_000_007;
+
+    public int minNonZeroProduct(int p) {
+        
+        if (p == 1)
+            return 1;
+
+        long mx = (long) (Math.pow(2, p)) - 1;
+        long sm = mx - 1;
+        long n = sm / 2;
+        long sum = rec(sm, n);
+
+        return (int) (sum * (mx % mod) % mod);
+    }
+
+    public long rec(long val, long n) {
+        
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return (val % mod);
+
+        long newVal = ((val % mod) * (val % mod)) % mod;
+
+        if (n % 2 != 0) {
+            return ((rec(newVal, n / 2) % mod) * (val % mod)) % mod;
+        }
+        return rec(newVal, n / 2) % mod;
+    }
 }
