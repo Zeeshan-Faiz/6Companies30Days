8e6bc024e9fe253888b9ed0d096cf15b3d8ba353
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-28 22:20:46 +0530

    Added Q1994NumberOfGoodSubsets code

--- Atlassian/Q1994NumberOfGoodSubsets.java
+++ Atlassian/Q1994NumberOfGoodSubsets.java
@@ -12,7 +12,25 @@ A subset of nums is any array that can be obtained by deleting some (possibly no
 from nums. Two subsets are different if and only if the chosen indices to delete are different.
 
 Example 1:
+Input: nums = [1,2,3,4]
+Output: 6
+Explanation: The good subsets are:
+- [1,2]: product is 2, which is the product of distinct prime 2.
+- [1,2,3]: product is 6, which is the product of distinct primes 2 and 3.
+- [1,3]: product is 3, which is the product of distinct prime 3.
+- [2]: product is 2, which is the product of distinct prime 2.
+- [2,3]: product is 6, which is the product of distinct primes 2 and 3.
+- [3]: product is 3, which is the product of distinct prime 3.
 
+Example 2:
+Input: nums = [4,2,3,15]
+Output: 5
+Explanation: The good subsets are:
+- [2]: product is 2, which is the product of distinct prime 2.
+- [2,3]: product is 6, which is the product of distinct primes 2 and 3.
+- [2,15]: product is 30, which is the product of distinct primes 2, 3, and 5.
+- [3]: product is 3, which is the product of distinct prime 3.
+- [15]: product is 15, which is the product of distinct primes 3 and 5.
 */
 
 public class Q1994NumberOfGoodSubsets {
