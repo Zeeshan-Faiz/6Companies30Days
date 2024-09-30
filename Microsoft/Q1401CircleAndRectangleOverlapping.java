3819be79e10c22a73b8cd4dfa6bffeb1a7dfc12b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-09-30 17:16:59 +0530

    Added Q1401CircleAndRectangleOverlapping code

--- Microsoft/Q1401CircleAndRectangleOverlapping.java
+++ Microsoft/Q1401CircleAndRectangleOverlapping.java
@@ -1,5 +1,16 @@
 package Microsoft;
 
+/*
+You are given a circle represented as (radius, xCenter, yCenter) and an axis-aligned rectangle 
+represented as (x1, y1, x2, y2), where (x1, y1) are the coordinates of the bottom-left corner, 
+and (x2, y2) are the coordinates of the top-right corner of the rectangle.
+Return true if the circle and rectangle are overlapped otherwise return false. In other words, 
+check if there is any point (xi, yi) that belongs to the circle and the rectangle at the same time.
+
+Example 1:
+
+*/
+
 public class Q1401CircleAndRectangleOverlapping {
     
 }
