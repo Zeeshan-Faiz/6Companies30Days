adf9386f7bd277ccc9f5f4f6a60824cbd355f0ae
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-01 10:12:34 +0530

    Added Q2976MinCostToConvertString code

--- Microsoft/Q2976MinCostToConvertString.java
+++ Microsoft/Q2976MinCostToConvertString.java
@@ -13,7 +13,25 @@ operations. If it is impossible to convert source to target, return -1.
 Note that there may exist indices i, j such that original[j] == original[i] and changed[j] == changed[i].
 
 Example 1:
+Input: source = "abcd", target = "acbe", original = ["a","b","c","c","e","d"], changed = ["b","c","b","e","b","e"], cost = [2,5,5,1,2,20]
+Output: 28
+Explanation: To convert the string "abcd" to string "acbe":
+- Change value at index 1 from 'b' to 'c' at a cost of 5.
+- Change value at index 2 from 'c' to 'e' at a cost of 1.
+- Change value at index 2 from 'e' to 'b' at a cost of 2.
+- Change value at index 3 from 'd' to 'e' at a cost of 20.
+The total cost incurred is 5 + 1 + 2 + 20 = 28.
+It can be shown that this is the minimum possible cost.
 
+Example 2:
+Input: source = "aaaa", target = "bbbb", original = ["a","c"], changed = ["c","b"], cost = [1,2]
+Output: 12
+Explanation: To change the character 'a' to 'b' change the character 'a' to 'c' at a cost of 1, followed by changing the character 'c' to 'b' at a cost of 2, for a total cost of 1 + 2 = 3. To change all occurrences of 'a' to 'b', a total cost of 3 * 4 = 12 is incurred.
+
+Example 3:
+Input: source = "abcd", target = "abce", original = ["a"], changed = ["e"], cost = [10000]
+Output: -1
+Explanation: It is impossible to convert source to target because the value at index 3 cannot be changed from 'd' to 'e'.
 */
 
 public class Q2976MinCostToConvertString {
