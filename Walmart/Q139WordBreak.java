b4c157ab02ec1a5193c35629e855761bd8a25523
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-02 11:24:19 +0530

    Added Q139WordBreak code

--- Walmart/Q139WordBreak.java
+++ Walmart/Q139WordBreak.java
@@ -1,5 +1,7 @@
 package Walmart;
 
+import java.util.List;
+
 /*
 Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a 
 space-separated sequence of one or more dictionary words.
@@ -23,4 +25,42 @@ Output: false
 
 public class Q139WordBreak {
     
+    public boolean wordBreak(String s, List<String> wordDict) {
+        //create array for memo;
+        Boolean[] arr = new Boolean[s.length()];
+        return helper(s, wordDict, 0, arr);
+    }
+    public boolean helper(String s, List<String> wordDict, int index, Boolean[] arr) {
+
+        if(index >= s.length()) {
+            return true;
+        }
+        if(arr[index] != null) {
+            return arr[index];
+        }
+        //interate through wordDict
+        for(int i=0; i<wordDict.size(); i++) {
+            String word = wordDict.get(i);
+            if(s.length()-index < word.length() || s.charAt(index) != word.charAt(0)) {
+                continue;
+            }
+            //iterate through the word
+            int j = 0;
+            while(j<word.length()) {
+                if(s.charAt(index+j) != word.charAt(j)) {
+                    break;
+                }
+                j++;
+            }
+            //if whold word found in this portion of "s". 
+            if(j == word.length()) {
+                arr[index] = helper(s, wordDict, index+j, arr);
+                if(arr[index] == true) {
+                    return true;
+                }
+            }
+        }
+        //return false if none of the word in wordDict can be found in this portion of "s".
+        return false;
+    }
 }
