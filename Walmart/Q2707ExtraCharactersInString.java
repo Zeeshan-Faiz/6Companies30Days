ba60b8c1fa0a66b0b1c1bf173b1ef636eef992fa
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-03 18:48:36 +0530

    Added Q2707ExtraCharactersInString code

--- Walmart/Q2707ExtraCharactersInString.java
+++ Walmart/Q2707ExtraCharactersInString.java
@@ -1,5 +1,15 @@
 package Walmart;
 
+/*
+You are given a 0-indexed string s and a dictionary of words dictionary. You have to break s into 
+one or more non-overlapping substrings such that each substring is present in dictionary. There 
+may be some extra characters in s which are not present in any of the substrings.
+Return the minimum number of extra characters left over if you break up s optimally.
+
+Example 1:
+
+*/
+
 public class Q2707ExtraCharactersInString {
     
 }
