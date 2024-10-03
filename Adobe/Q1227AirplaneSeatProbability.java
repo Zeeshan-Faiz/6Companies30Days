093fec5ae39b8b9b187640a80621150ecfada63b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-03 18:50:53 +0530

    Added Q1227AirplaneSeatProbability code

--- Adobe/Q1227AirplaneSeatProbability.java
+++ Adobe/Q1227AirplaneSeatProbability.java
@@ -1,5 +1,17 @@
 package Adobe;
 
+/*
+n passengers board an airplane with exactly n seats. The first passenger has lost the ticket and 
+picks a seat randomly. But after that, the rest of the passengers will:
+    Take their own seat if it is still available, and
+    Pick other seats randomly when they find their seat occupied
+
+Return the probability that the nth person gets his own seat.
+
+Example 1:
+
+*/
+
 public class Q1227AirplaneSeatProbability {
     
 }
