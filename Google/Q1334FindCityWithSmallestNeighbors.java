fded82720e921c0144c9054fdf8a1588b38b9558
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-04 15:38:50 +0530

    Added Q1334FindCityWithSmallestNeighbors code

--- Google/Q1334FindCityWithSmallestNeighbors.java
+++ Google/Q1334FindCityWithSmallestNeighbors.java
@@ -1,5 +1,19 @@
 package Google;
 
+/*
+There are n cities numbered from 0 to n-1. Given the array edges where edges[i] = 
+[fromi, toi, weighti] represents a bidirectional and weighted edge between cities fromi and toi, 
+and given the integer distanceThreshold.
+Return the city with the smallest number of cities that are reachable through some path and whose 
+distance is at most distanceThreshold, If there are multiple such cities, return the city with 
+the greatest number.
+Notice that the distance of a path connecting cities i and j is equal to the sum of the edges' 
+weights along that path.
+
+Example 1:
+
+*/
+
 public class Q1334FindCityWithSmallestNeighbors {
     
 }
