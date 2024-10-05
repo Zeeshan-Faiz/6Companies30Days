b4a312ac3ee69e2c9a1dc8177ab63e8acfa29020
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-05 14:41:38 +0530

    Added Q23MergeKSortedLists code

--- Google/Q23MergeKSortedLists.java
+++ Google/Q23MergeKSortedLists.java
@@ -5,7 +5,24 @@ You are given an array of k linked-lists lists, each linked-list is sorted in as
 Merge all the linked-lists into one sorted linked-list and return it.
 
 Example 1:
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
 
+Example 2:
+Input: lists = []
+Output: []
+
+Example 3:
+Input: lists = [[]]
+Output: []
 */
 
 public class Q23MergeKSortedLists {
