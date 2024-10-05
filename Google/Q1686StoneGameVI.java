50e41f903afc90940058face5e34ba37706e581e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-05 14:40:35 +0530

    Added Q1686StoneGameVI code

--- Google/Q1686StoneGameVI.java
+++ Google/Q1686StoneGameVI.java
@@ -1,5 +1,8 @@
 package Google;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Alice and Bob take turns playing a game, with Alice starting first.
 There are n stones in a pile. On each player's turn, they can remove a stone from the pile and 
@@ -39,5 +42,50 @@ Bob wins.
 */
 
 public class Q1686StoneGameVI {
-    
+
+    public int stoneGameVI(int[] av, int[] bv) {
+        
+        @SuppressWarnings("unchecked")
+        List<Integer> a[] = new ArrayList[201];
+        int n = av.length;
+        for (int i = 0; i < n; i++) {
+            int sum = av[i] + bv[i];
+            if (a[sum] == null) {
+                a[sum] = new ArrayList<Integer>();
+                a[sum].add(i);
+            } else {
+                a[sum].add(i);
+            }
+        }
+
+        int[] aa = new int[n];
+        int x = 0;
+        for (int i = 200; i >= 0; i--) {
+            if (a[i] != null) {
+                for (int p : a[i]) {
+                    aa[x] = p;
+                    x++;
+                }
+            }
+        }
+
+        int as = 0;
+        int bs = 0;
+        int xx = 0;
+        while (xx < n) {
+            as += av[aa[xx]];
+            if (xx + 1 < n) {
+                bs += bv[aa[xx + 1]];
+            }
+            xx += 2;
+        }
+
+        if (as > bs) {
+            return 1;
+        }
+        if (bs > as) {
+            return -1;
+        }
+        return 0;
+    }
 }
