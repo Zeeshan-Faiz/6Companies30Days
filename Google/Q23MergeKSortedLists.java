239ee90ed2f42b185b5512d93502d91dcbb71d23
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-05 14:42:19 +0530

    Added Q23MergeKSortedLists code

--- Google/Q23MergeKSortedLists.java
+++ Google/Q23MergeKSortedLists.java
@@ -26,27 +26,30 @@ Output: []
 */
 
 public class Q23MergeKSortedLists {
-    
+
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0) return null;
-        return helper(lists,0,lists.length-1);
+        if (lists.length == 0)
+            return null;
+        return helper(lists, 0, lists.length - 1);
     }
 
-    public ListNode helper(ListNode[] lists, int start,int end) {
-        if(start == end) return lists[start];
-        if(start == end-1) return merge(lists[start],lists[end]);
-        int mid = start + (end-start) / 2;
-        ListNode left = helper(lists,start,mid);
-        ListNode right = helper(lists,mid+1,end);
-        return merge(left,right);
+    public ListNode helper(ListNode[] lists, int start, int end) {
+        if (start == end)
+            return lists[start];
+        if (start == end - 1)
+            return merge(lists[start], lists[end]);
+        int mid = start + (end - start) / 2;
+        ListNode left = helper(lists, start, mid);
+        ListNode right = helper(lists, mid + 1, end);
+        return merge(left, right);
     }
 
     public ListNode merge(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(-1);
         ListNode temp = head;
-        
-        while(l1!=null && l2!=null) {
-            if(l1.val>l2.val) {
+
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
                 temp.next = new ListNode(l2.val);
                 l2 = l2.next;
             } else {
@@ -57,5 +60,22 @@ public class Q23MergeKSortedLists {
         }
         temp.next = l1 != null ? l1 : l2;
         return head.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
