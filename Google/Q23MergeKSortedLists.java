126673fa720b23ca01ea36c1feb3bfd9043a38dc
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-05 14:41:28 +0530

    Added Q23MergeKSortedLists code

--- Google/Q23MergeKSortedLists.java
+++ Google/Q23MergeKSortedLists.java
@@ -1,5 +1,13 @@
 package Google;
 
+/*
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+Merge all the linked-lists into one sorted linked-list and return it.
+
+Example 1:
+
+*/
+
 public class Q23MergeKSortedLists {
     
 }
