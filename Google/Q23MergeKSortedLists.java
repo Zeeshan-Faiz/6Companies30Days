f3d6362c6189fcda38e4fd9ac3e222131666bf55
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-05 14:42:01 +0530

    Added Q23MergeKSortedLists code

--- Google/Q23MergeKSortedLists.java
+++ Google/Q23MergeKSortedLists.java
@@ -27,4 +27,35 @@ Output: []
 
 public class Q23MergeKSortedLists {
     
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0) return null;
+        return helper(lists,0,lists.length-1);
+    }
+
+    public ListNode helper(ListNode[] lists, int start,int end) {
+        if(start == end) return lists[start];
+        if(start == end-1) return merge(lists[start],lists[end]);
+        int mid = start + (end-start) / 2;
+        ListNode left = helper(lists,start,mid);
+        ListNode right = helper(lists,mid+1,end);
+        return merge(left,right);
+    }
+
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode temp = head;
+        
+        while(l1!=null && l2!=null) {
+            if(l1.val>l2.val) {
+                temp.next = new ListNode(l2.val);
+                l2 = l2.next;
+            } else {
+                temp.next = new ListNode(l1.val);
+                l1 = l1.next;
+            }
+            temp = temp.next;
+        }
+        temp.next = l1 != null ? l1 : l2;
+        return head.next;
+    }
 }
