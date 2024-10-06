14755d3d6216ef497474bb162f160467103dfc9e
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-06 10:39:55 +0530

    Added Q2233MaxProductAfterKIncrements code

--- Google/Q2233MaxProductAfterKIncrements.java
+++ Google/Q2233MaxProductAfterKIncrements.java
@@ -1,5 +1,15 @@
 package Google;
 
+/*
+You are given an array of non-negative integers nums and an integer k. In one operation, you may 
+choose any element from nums and increment it by 1.
+Return the maximum product of nums after at most k operations. Since the answer may be very large, 
+return it modulo 109 + 7. Note that you should maximize the product before taking the modulo. 
+
+Example 1:
+
+*/
+
 public class Q2233MaxProductAfterKIncrements {
     
 }
