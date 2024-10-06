83511ec72b6e05c04ae892f731528a10c96cfba7
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-06 10:43:04 +0530

    Added Q211DesignWordDataStructure code

--- Google/Q211DesignWordDataStructure.java
+++ Google/Q211DesignWordDataStructure.java
@@ -9,7 +9,21 @@ Implement the WordDictionary class:
     bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
 
 Example 1:
+Input
+["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+Output
+[null,null,null,null,false,true,true,true]
 
+Explanation
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("bad");
+wordDictionary.addWord("dad");
+wordDictionary.addWord("mad");
+wordDictionary.search("pad"); // return False
+wordDictionary.search("bad"); // return True
+wordDictionary.search(".ad"); // return True
+wordDictionary.search("b.."); // return True
 */
 
 public class Q211DesignWordDataStructure {
