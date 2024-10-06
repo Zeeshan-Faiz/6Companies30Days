98684ba32e88d48cdda7543542315ba77d608e3c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-06 10:40:55 +0530

    Added Q2233MaxProductAfterKIncrements code

--- Google/Q2233MaxProductAfterKIncrements.java
+++ Google/Q2233MaxProductAfterKIncrements.java
@@ -1,5 +1,7 @@
 package Google;
 
+import java.util.Arrays;
+
 /*
 You are given an array of non-negative integers nums and an integer k. In one operation, you may 
 choose any element from nums and increment it by 1.
@@ -24,5 +26,54 @@ Note that there may be other ways to increment nums to have the maximum product.
 */
 
 public class Q2233MaxProductAfterKIncrements {
-    
+
+    public int maximumProduct(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        long product = 1;
+        long mod = (long) (1e9 + 7);
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                int diff = nums[i] - nums[i - 1];
+                if (diff * count <= k) {
+                    for (int j = 0; j < i; j++) {
+                        nums[j] += diff;
+                    }
+                    k -= count * diff;
+                    count++;
+                } else {
+                    int x = k / count;
+                    int y = k % count;
+                    for (int j = 0; j < i; j++) {
+                        nums[j] += x;
+                    }
+                    for (int j = 0; j < y; j++) {
+                        nums[i - 1 - j]++;
+                    }
+                    count = 0;
+                    k = 0;
+                }
+            }
+            if (k == 0) {
+                break;
+            }
+        }
+        if (count >= 1 && k != 0) {
+            int x = k / n;
+            int y = k % n;
+            for (int j = 0; j < n; j++) {
+                if (j < y) {
+                    nums[j]++;
+                }
+                nums[j] += x;
+            }
+        }
+        for (int i : nums) {
+            product = (product * ((long) i)) % mod;
+        }
+        return (int) (product);
+    }
 }
