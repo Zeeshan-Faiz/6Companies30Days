e971a8b31ad66cd239d88df04a4dac6bbfcd7b81
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-07 16:18:00 +0530

    Added Q1095FoundInMountainArray code

--- Google/Q1095FoundInMountainArray.java
+++ Google/Q1095FoundInMountainArray.java
@@ -29,4 +29,60 @@ Explanation: 3 does not exist in the array, so we return -1.
 
 public class Q1095FoundInMountainArray {
     
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        
+        int peak = peakIndex(mountainArr);
+
+        int firstTry = agnosticArray(mountainArr, target,0,peak);
+        if(firstTry != -1)
+            return firstTry;
+        else
+            return agnosticArray(mountainArr, target, peak+1, mountainArr.length()-1);
+
+    }
+
+    public int peakIndex(MountainArray mountainArr){
+
+        int start = 0;
+        int end = mountainArr.length() - 1;
+        int mid = -1;
+
+        while(start < end){
+            
+            mid = (start+end)/2;
+            if(mountainArr.get(mid) > mountainArr.get(mid+1))
+                //we are in descending part
+                end = mid;
+            else
+                //we are in ascending part
+                start = mid + 1;
+        }
+        return start;
+    }
+
+    public int agnosticArray(MountainArray mountainArr, int target, int start, int end){
+
+        //check whether the given array is Ascending or Descending.
+        boolean isAsc = mountainArr.get(start) < mountainArr.get(end);
+        
+        while(start <= end){
+            int mid = (start+end)/2;
+            if(mountainArr.get(mid) == target)
+                return mid;
+            
+            if(isAsc){
+                if(mountainArr.get(mid) < target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            else{
+                if(mountainArr.get(mid) > target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
