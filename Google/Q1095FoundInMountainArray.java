2e0f91476e83da52b5ab3a7121a2f13de1723f6d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-07 16:23:06 +0530

    Added Q1095FoundInMountainArray code

--- Google/Q1095FoundInMountainArray.java
+++ Google/Q1095FoundInMountainArray.java
@@ -37,20 +37,20 @@ public class Q1095FoundInMountainArray {
         if (firstTry != -1)
             return firstTry;
         else
-            return agnosticArray(mountainArr, target, peak + 1, mountainArr.length() - 1);
+            return agnosticArray(mountainArr, target, peak + 1, MountainArray.length() - 1);
 
     }
 
     public int peakIndex(MountainArray mountainArr) {
 
         int start = 0;
-        int end = mountainArr.length() - 1;
+        int end = MountainArray.length() - 1;
         int mid = -1;
 
         while (start < end) {
 
             mid = (start + end) / 2;
-            if (mountainArr.get(mid) > mountainArr.get(mid + 1))
+            if (MountainArray.get(mid) > MountainArray.get(mid + 1))
                 // we are in descending part
                 end = mid;
             else
@@ -63,25 +63,31 @@ public class Q1095FoundInMountainArray {
     public int agnosticArray(MountainArray mountainArr, int target, int start, int end) {
 
         // check whether the given array is Ascending or Descending.
-        boolean isAsc = mountainArr.get(start) < mountainArr.get(end);
+        boolean isAsc = MountainArray.get(start) < MountainArray.get(end);
 
         while (start <= end) {
             int mid = (start + end) / 2;
-            if (mountainArr.get(mid) == target)
+            if (MountainArray.get(mid) == target)
                 return mid;
 
             if (isAsc) {
-                if (mountainArr.get(mid) < target)
+                if (MountainArray.get(mid) < target)
                     start = mid + 1;
                 else
                     end = mid - 1;
             } else {
-                if (mountainArr.get(mid) > target)
+                if (MountainArray.get(mid) > target)
                     start = mid + 1;
                 else
                     end = mid - 1;
             }
         }
         return -1;
+    }
+
+    interface MountainArray {
+        public static int get(int index) { return index;}
+
+        public static int length() { return 0;}
     }
 }
