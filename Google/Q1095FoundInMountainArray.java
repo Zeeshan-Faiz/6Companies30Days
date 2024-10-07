0ad7bccb23423ab291e7ad9b93d5b737458c315f
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-07 16:17:43 +0530

    Added Q1095FoundInMountainArray code

--- Google/Q1095FoundInMountainArray.java
+++ Google/Q1095FoundInMountainArray.java
@@ -17,7 +17,14 @@ Submissions making more than 100 calls to MountainArray.get will be judged Wrong
 any solutions that attempt to circumvent the judge will result in disqualification.
 
 Example 1:
+Input: array = [1,2,3,4,5,3,1], target = 3
+Output: 2
+Explanation: 3 exists in the array, at index=2 and index=5. Return the minimum index, which is 2.
 
+Example 2:
+Input: array = [0,1,2,4,2,1], target = 3
+Output: -1
+Explanation: 3 does not exist in the array, so we return -1.
 */
 
 public class Q1095FoundInMountainArray {
