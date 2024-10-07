33cdb4ad670d995a71a8c3764e5bf231194137e5
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-07 16:15:08 +0530

    Added Q1530NoOfGoodLeafPairs code

--- Google/Q1530NoOfGoodLeafPairs.java
+++ Google/Q1530NoOfGoodLeafPairs.java
@@ -1,5 +1,14 @@
 package Google;
 
+/*
+You are given the root of a binary tree and an integer distance. A pair of two different leaf 
+nodes of a binary tree is said to be good if the length of the shortest path between them is 
+less than or equal to distance. Return the number of good leaf node pairs in the tree.
+
+Example 1:
+
+*/
+
 public class Q1530NoOfGoodLeafPairs {
     
 }
