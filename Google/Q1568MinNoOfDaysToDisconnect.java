4923dff9cde576a3edccc9e761dc92169eeb5d5c
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-08 19:07:14 +0530

    Added Q1568MinNoOfDaysToDisconnect code

--- Google/Q1568MinNoOfDaysToDisconnect.java
+++ Google/Q1568MinNoOfDaysToDisconnect.java
@@ -22,4 +22,86 @@ Explanation: Grid of full water is also disconnected ([[1,1]] -> [[0,0]]), 0 isl
 
 public class Q1568MinNoOfDaysToDisconnect {
     
+    public int minDays(int[][] grid) {
+        
+        int[][] times = new int[grid.length][grid[0].length];
+        boolean seen = false;
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                    if (times[i][j] == 0) {
+                        if (seen)
+                            return 0;
+                        seen = true;
+                        time = 1;
+                        dfs(grid, times, i, j);
+                    }
+                }
+            }
+        }
+        if (count <= 2)
+            return count;
+        if (found)
+            return 1;
+        return 2;
+    }
+
+    int time = 1;
+    boolean found = false;
+
+    public int dfs(int[][] grid, int[][] times, int i, int j) {
+        times[i][j] = time;
+        int res = time++;
+        if (i > 0 && grid[i - 1][j] == 1) {
+            if (times[i - 1][j] != 0) {
+                if (times[i - 1][j] != times[i][j] - 1)
+                    res = times[i - 1][j];
+            } else {
+                int min = dfs(grid, times, i - 1, j);
+                if (min > times[i][j] || (times[i][j] > 1 && min == times[i][j])) {
+                    found = true;
+                }
+                res = min;
+            }
+        }
+        if (i < grid.length - 1 && grid[i + 1][j] == 1) {
+            if (times[i + 1][j] != 0) {
+                if (times[i + 1][j] != times[i][j] - 1)
+                    res = Math.min(times[i + 1][j], res);
+            } else {
+                int min = dfs(grid, times, i + 1, j);
+                if (min > times[i][j] || (times[i][j] > 1 && min == times[i][j])) {
+                    found = true;
+                }
+                res = Math.min(min, res);
+            }
+        }
+        if (j > 0 && grid[i][j - 1] == 1) {
+            if (times[i][j - 1] != 0) {
+                if (times[i][j - 1] != times[i][j] - 1)
+                    res = Math.min(times[i][j - 1], res);
+            } else {
+                int min = dfs(grid, times, i, j - 1);
+                if (min > times[i][j] || (times[i][j] > 1 && min == times[i][j])) {
+                    found = true;
+                }
+                res = Math.min(min, res);
+            }
+        }
+        if (j < grid[0].length - 1 && grid[i][j + 1] == 1) {
+            if (times[i][j + 1] != 0) {
+                if (times[i][j + 1] != times[i][j] - 1)
+                    res = Math.min(times[i][j + 1], res);
+            } else {
+                int min = dfs(grid, times, i, j + 1);
+                if (min > times[i][j] || (times[i][j] > 1 && min == times[i][j])) {
+                    found = true;
+                }
+                res = Math.min(min, res);
+            }
+        }
+        return res;
+    }
 }
