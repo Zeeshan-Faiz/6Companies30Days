06ea8f717de39ba4f3538245cf887e8d9639ff22
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-08 19:06:52 +0530

    Added Q1568MinNoOfDaysToDisconnect code

--- Google/Q1568MinNoOfDaysToDisconnect.java
+++ Google/Q1568MinNoOfDaysToDisconnect.java
@@ -8,7 +8,16 @@ one day, we are allowed to change any single land cell (1) into a water cell (0)
 Return the minimum number of days to disconnect the grid.
 
 Example 1:
+Input: grid = [[0,1,1,0],[0,1,1,0],[0,0,0,0]]
 
+Output: 2
+Explanation: We need at least 2 days to get a disconnected grid.
+Change land grid[1][1] and grid[0][2] to water and get 2 disconnected island.
+
+Example 2:
+Input: grid = [[1,1]]
+Output: 2
+Explanation: Grid of full water is also disconnected ([[1,1]] -> [[0,0]]), 0 islands.
 */
 
 public class Q1568MinNoOfDaysToDisconnect {
