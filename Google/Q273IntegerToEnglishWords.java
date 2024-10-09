5ddf6c72a0ea030a23288f25beeb8382b2eb2920
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-09 10:07:02 +0530

    Added Q273IntegerToEnglishWords code

--- Google/Q273IntegerToEnglishWords.java
+++ Google/Q273IntegerToEnglishWords.java
@@ -4,7 +4,16 @@ package Google;
 Convert a non-negative integer num to its English words representation.
 
 Example 1:
+Input: num = 123
+Output: "One Hundred Twenty Three"
 
+Example 2:
+Input: num = 12345
+Output: "Twelve Thousand Three Hundred Forty Five"
+
+Example 3:
+Input: num = 1234567
+Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
 */
 
 public class Q273IntegerToEnglishWords {
