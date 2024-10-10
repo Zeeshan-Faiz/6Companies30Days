67e45fd16b03270a03a03cf479f9b7367132095d
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-10 19:00:41 +0530

    Added Q2223SumOfScoresOfBuiltStrings code

--- Google/Q2223SumOfScoresOfBuiltStrings.java
+++ Google/Q2223SumOfScoresOfBuiltStrings.java
@@ -10,7 +10,25 @@ The score of si is the length of the longest common prefix between si and sn (No
 Given the final string s, return the sum of the score of every si.
 
 Example 1:
+Input: s = "babab"
+Output: 9
+Explanation:
+For s1 == "b", the longest common prefix is "b" which has a score of 1.
+For s2 == "ab", there is no common prefix so the score is 0.
+For s3 == "bab", the longest common prefix is "bab" which has a score of 3.
+For s4 == "abab", there is no common prefix so the score is 0.
+For s5 == "babab", the longest common prefix is "babab" which has a score of 5.
+The sum of the scores is 1 + 0 + 3 + 0 + 5 = 9, so we return 9.
 
+Example 2:
+Input: s = "azbazbzaz"
+Output: 14
+Explanation: 
+For s2 == "az", the longest common prefix is "az" which has a score of 2.
+For s6 == "azbzaz", the longest common prefix is "azb" which has a score of 3.
+For s9 == "azbazbzaz", the longest common prefix is "azbazbzaz" which has a score of 9.
+For all other si, the score is 0.
+The sum of the scores is 2 + 3 + 9 = 14, so we return 14.
 */
 
 public class Q2223SumOfScoresOfBuiltStrings {
