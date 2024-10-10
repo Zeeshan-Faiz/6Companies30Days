c6a3795b530942d590d3e645cd4dd7cb65a7d3a9
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-10 19:00:29 +0530

    Added Q2223SumOfScoresOfBuiltStrings code

--- Google/Q2223SumOfScoresOfBuiltStrings.java
+++ Google/Q2223SumOfScoresOfBuiltStrings.java
@@ -1,5 +1,18 @@
 package Google;
 
+/*
+You are building a string s of length n one character at a time, prepending each new character to 
+the front of the string. The strings are labeled from 1 to n, where the string with length i is 
+labeled si.
+    For example, for s = "abaca", s1 == "a", s2 == "ca", s3 == "aca", etc.
+
+The score of si is the length of the longest common prefix between si and sn (Note that s == sn).
+Given the final string s, return the sum of the score of every si.
+
+Example 1:
+
+*/
+
 public class Q2223SumOfScoresOfBuiltStrings {
     
 }
