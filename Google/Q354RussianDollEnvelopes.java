30274df69f06223717529d883e5be535145f3d57
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-11 18:56:44 +0530

    Added Q354RussianDollEnvelopes code

--- Google/Q354RussianDollEnvelopes.java
+++ Google/Q354RussianDollEnvelopes.java
@@ -1,5 +1,17 @@
 package Google;
 
+/*
+You are given a 2D array of integers envelopes where envelopes[i] = [wi, hi] represents the width 
+and the height of an envelope.
+One envelope can fit into another if and only if both the width and height of one envelope are 
+greater than the other envelope's width and height.
+Return the maximum number of envelopes you can Russian doll (i.e., put one inside the other).
+Note: You cannot rotate an envelope.
+
+Example 1:
+
+*/
+
 public class Q354RussianDollEnvelopes {
     
 }
