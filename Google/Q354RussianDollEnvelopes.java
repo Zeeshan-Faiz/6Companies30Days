3c3765b13e0ab114f7c74d2e3074cf84155ea4fd
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-11 18:58:39 +0530

    Added Q354RussianDollEnvelopes code

--- Google/Q354RussianDollEnvelopes.java
+++ Google/Q354RussianDollEnvelopes.java
@@ -1,5 +1,7 @@
 package Google;
 
+import java.util.Arrays;
+
 /*
 You are given a 2D array of integers envelopes where envelopes[i] = [wi, hi] represents the width 
 and the height of an envelope.
@@ -19,5 +21,34 @@ Output: 1
 */
 
 public class Q354RussianDollEnvelopes {
-    
+
+    public int maxEnvelopes(int[][] envelopes) {
+
+        Arrays.sort(envelopes, (a, b) -> {
+            return a[0] == b[0] ? b[1] - a[1] : a[0] - b[0];
+        });
+        int n = envelopes.length;
+        int[] d = new int[n + 1];
+        d[1] = envelopes[0][1];
+        int size = 1;
+        for (int i = 1; i < n; ++i) {
+            int x = envelopes[i][1];
+            if (x > d[size]) {
+                d[++size] = x;
+            } else {
+                int left = 1, right = size;
+                while (left < right) {
+                    int mid = (left + right) >> 1;
+                    if (d[mid] >= x) {
+                        right = mid;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+                int p = d[left] >= x ? left : 1;
+                d[p] = x;
+            }
+        }
+        return size;
+    }
 }
