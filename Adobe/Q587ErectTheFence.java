2207705035cc605b4168d72f9dc71b160fef74a3
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-14 15:45:58 +0530

    Added Q587ErectTheFence code

--- Adobe/Q587ErectTheFence.java
+++ Adobe/Q587ErectTheFence.java
@@ -8,7 +8,14 @@ Return the coordinates of trees that are exactly located on the fence perimeter.
 the answer in any order.
 
 Example 1:
+Input: trees = [[1,1],[2,2],[2,0],[2,4],[3,3],[4,2]]
+Output: [[1,1],[2,0],[4,2],[3,3],[2,4]]
+Explanation: All the trees will be on the perimeter of the fence except the tree at [2, 2], which will be inside the fence.
 
+Example 2:
+Input: trees = [[1,2],[2,2],[4,2]]
+Output: [[4,2],[2,2],[1,2]]
+Explanation: The fence forms a line that passes through all the trees.
 */
 
 public class Q587ErectTheFence {
