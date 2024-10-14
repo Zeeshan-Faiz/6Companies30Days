850ccf0c6a35329ea292ad8cb3db7b0110b8cb6b
Zeeshan-Faiz <Md.Zeeshan.Faiz7@gmail.com>
2024-10-14 15:45:45 +0530

    Added Q587ErectTheFence code

--- Adobe/Q587ErectTheFence.java
+++ Adobe/Q587ErectTheFence.java
@@ -1,5 +1,16 @@
 package Adobe;
 
+/*
+You are given an array trees where trees[i] = [xi, yi] represents the location of a tree in the garden.
+Fence the entire garden using the minimum length of rope, as it is expensive. The garden is well-fenced 
+only if all the trees are enclosed.
+Return the coordinates of trees that are exactly located on the fence perimeter. You may return 
+the answer in any order.
+
+Example 1:
+
+*/
+
 public class Q587ErectTheFence {
     
 }
